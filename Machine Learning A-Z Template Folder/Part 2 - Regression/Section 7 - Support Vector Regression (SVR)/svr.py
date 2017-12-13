c71da6827f3ff50296376edccec769a302cf1b75
Hyunkoo <knohyou@gmail.com>
2017-12-13 18:50:29 -0500

    Completed Part2 SVR in Python

---
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
@@ -0,0 +1,55 @@
+# Regression Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X = sc_X.fit_transform(X)
+sc_y = StandardScaler()
+y = sc_y.fit_transform(y)
+
+# Fitting the Regression Model to the dataset Fitting SVR to the dataset
+# Create your regressor here
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+# Know our problem is non-linear so choose either poly or rbf (Gaussian kernel)
+# Fit regressor to the X dataset
+regressor.fit(X,y)￼
+
+# Predicting a new result
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
+# Two brackets to create an array
+# One bracket indicates a vector
+# Need to inverse transform to get origin
+
+
+# Visualising the Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')￼
+plt.show()
+
+# Visualising the Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (SVR Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
