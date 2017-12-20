3f86bd6eda6d71748d4cd9e1d5208e9a74f73327
Hyunkoo <knohyou@gmail.com>
2017-12-19 23:40:59 -0500

    Completed Part3 Logistic Regression in Python

---
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 14 - Logistic Regression/Logistic_Regression/logistic_regression.py
@@ -0,0 +1,45 @@
+# Logistic Regresion
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, 2:4].values # or [:, [2,3]]
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+#sc_y = StandardScaler()
+#y_train = sc_y.fit_transform(y_train)
+
+# Fitting the Regression Model to the dataset
+# Create your regressor here
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting a new result
+y_pred = classifier.predict(X_test)
+
+# Evaluate using the Confusion Matrix - Correct prediction 
+from sklearn.metrics import confusion_matrix # Class is capitalized. function is lowercase
+cm = confusion_matrix(y_test, y_pred)
+
+# Visualize the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1, stop = X_set[:,0].max()+1, step = 0.01),
+                     np.arange(start = X_set[:,1].min()-1, stop = X_set[:,1].max()+1, step = 0.01))
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), 
+             alpha = 0.75, cmap = ListedColormap(('red','green')))           
+plt.show()
