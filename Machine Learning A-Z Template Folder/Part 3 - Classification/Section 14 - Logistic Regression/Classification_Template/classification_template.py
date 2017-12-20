f624f74952683c6379b9279a4b0ff93b9d014393
Hyunkoo <knohyou@gmail.com>
2017-12-20 14:11:20 -0500

    Complted Par3 Logisitic Regression with R

--- Machine Learning A-Z Template Folder/Part 3 - Classification/Section 14 - Logistic Regression/Classification_Template/classification_template.py
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 14 - Logistic Regression/Classification_Template/classification_template.py
@@ -1,4 +1,4 @@
-# Classification template
+# Classification Template
 
 # Importing the libraries
 import numpy as np
@@ -7,7 +7,7 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
+X = dataset.iloc[:, 2:4].values # or [:, [2,3]]
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
@@ -16,52 +16,56 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, rand
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+#sc_y = StandardScaler()
+#y_train = sc_y.fit_transform(y_train)
 
-# Fitting classifier to the Training set
-# Create your classifier here
+# Fitting the Regression Model to the dataset
+# Create your regressor here
 
-# Predicting the Test set results
+
+# Predicting a new result
 y_pred = classifier.predict(X_test)
 
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
+# Evaluate using the Confusion Matrix - Correct prediction 
+from sklearn.metrics import confusion_matrix # Class is capitalized. function is lowercase
 cm = confusion_matrix(y_test, y_pred)
 
-# Visualising the Training set results
+# Visualize the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1, stop = X_set[:,0].max()+1, step = 0.01),
+                     np.arange(start = X_set[:,1].min()-1, stop = X_set[:,1].max()+1, step = 0.01))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),                                               
+             alpha = 0.75, cmap = ListedColormap(('red','green')))           
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Training set)')
-plt.xlabel('Age')
+                c = ListedColormap(('red','green'))(i), label = j)
 plt.ylabel('Estimated Salary')
+plt.xlabel('Age')
+plt.title('Logistic Regression (Training Set)')
 plt.legend()
 plt.show()
 
-# Visualising the Test set results
+# Visualize the Test Set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1, stop = X_set[:,0].max()+1, step = 0.01),
+                     np.arange(start = X_set[:,1].min()-1, stop = X_set[:,1].max()+1, step = 0.01))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),                                               
+             alpha = 0.75, cmap = ListedColormap(('red','green')))    
+       
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Test set)')
-plt.xlabel('Age')
+                c = ListedColormap(('red','green'))(i), label = j)
 plt.ylabel('Estimated Salary')
+plt.xlabel('Age')
+plt.title('Logistic Regression (Test Set)')
 plt.legend()
 plt.show()
--- Machine Learning A-Z Template Folder/Part 3 - Classification/Section 14 - Logistic Regression/Logistic_Regression/logistic_regression.py
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 14 - Logistic Regression/Logistic_Regression/logistic_regression.py
@@ -40,6 +40,34 @@ from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1, stop = X_set[:,0].max()+1, step = 0.01),
                      np.arange(start = X_set[:,1].min()-1, stop = X_set[:,1].max()+1, step = 0.01))
-plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), 
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),                                               
              alpha = 0.75, cmap = ListedColormap(('red','green')))           
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red','green'))(i), label = j)
+plt.ylabel('Estimated Salary')
+plt.xlabel('Age')
+plt.title('Logistic Regression (Training Set)')
+plt.legend()
+plt.show()
+
+# Visualize the Test Set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1, stop = X_set[:,0].max()+1, step = 0.01),
+                     np.arange(start = X_set[:,1].min()-1, stop = X_set[:,1].max()+1, step = 0.01))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),                                               
+             alpha = 0.75, cmap = ListedColormap(('red','green')))    
+       
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red','green'))(i), label = j)
+plt.ylabel('Estimated Salary')
+plt.xlabel('Age')
+plt.title('Logistic Regression (Test Set)')
+plt.legend()
 plt.show()
