beb5b3401015b25b3553c6631a7204e522861caa
Hyunkoo <knohyou@gmail.com>
2017-12-21 22:19:13 -0500

    Completed Part3 Decision Tree in R

--- Machine Learning A-Z Template Folder/Part 3 - Classification/Section 20 - Random Forest Classification/classification_template.R
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 20 - Random Forest Classification/classification_template.R
@@ -21,15 +21,9 @@ test_set[-3] = scale(test_set[-3])
 
 # Fitting classifier to the Training set
 # Create your classifier here
-library(rpart)
-classifier = rpart(formula = Purchased ~ ., 
-      data = training_set)
-
 
 # Predicting the Test set results
-#y_pred = predict(classifier, newdata = test_set[-3])
-#y_pred = ifelse(y_pred[,1]>0.5,0,1)
-y_pred = predict(classifier, newdata = test_set[-3], type = 'class')
+y_pred = predict(classifier, newdata = test_set[-3])
 
 # Making the Confusion Matrix
 cm = table(test_set[, 3], y_pred)
@@ -41,7 +35,7 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set, type = 'class')
+y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
      main = 'Classifier (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
@@ -57,7 +51,7 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set, type = 'class')
+y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3], main = 'Classifier (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
--- Machine Learning A-Z Template Folder/Part 3 - Classification/Section 20 - Random Forest Classification/random_forest_classification.R
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 20 - Random Forest Classification/random_forest_classification.R
@@ -1,4 +1,4 @@
-# Classification template
+# Random Forest Classification
 
 # Importing the dataset
 dataset = read.csv('Social_Network_Ads.csv')
@@ -19,17 +19,16 @@ test_set = subset(dataset, split == FALSE)
 training_set[-3] = scale(training_set[-3])
 test_set[-3] = scale(test_set[-3])
 
-# Fitting classifier to the Training set
-# Create your classifier here
-library(rpart)
-classifier = rpart(formula = Purchased ~ ., 
-      data = training_set)
-
+# Fitting Random Forest Classification to the Training set
+# install.packages('randomForest')
+library(randomForest)
+set.seed(123)
+classifier = randomForest(x = training_set[-3],
+                          y = training_set$Purchased,
+                          ntree = 500)
 
 # Predicting the Test set results
-#y_pred = predict(classifier, newdata = test_set[-3])
-#y_pred = ifelse(y_pred[,1]>0.5,0,1)
-y_pred = predict(classifier, newdata = test_set[-3], type = 'class')
+y_pred = predict(classifier, newdata = test_set[-3])
 
 # Making the Confusion Matrix
 cm = table(test_set[, 3], y_pred)
@@ -41,9 +40,9 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set, type = 'class')
+y_grid = predict(classifier, grid_set)
 plot(set[, -3],
-     main = 'Classifier (Training set)',
+     main = 'Random Forest Classification (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -57,10 +56,13 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set, type = 'class')
-plot(set[, -3], main = 'Classifier (Test set)',
+y_grid = predict(classifier, grid_set)
+plot(set[, -3], main = 'Random Forest Classification (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Choosing the number of trees
+plot(classifier)
