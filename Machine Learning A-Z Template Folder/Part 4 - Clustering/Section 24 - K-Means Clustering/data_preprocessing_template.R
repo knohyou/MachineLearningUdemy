fc5209d9fd29f53ce0aee025a0aa028c921653a2
Hyunkoo <knohyou@gmail.com>
2017-12-22 13:54:59 -0500

    Completed Part4 KMeans Clustering in R

--- Machine Learning A-Z Template Folder/Part 4 - Clustering/Section 24 - K-Means Clustering/data_preprocessing_template.R
+++ Machine Learning A-Z Template Folder/Part 4 - Clustering/Section 24 - K-Means Clustering/data_preprocessing_template.R
@@ -1,16 +1,35 @@
-# Data Preprocessing Template
+# KMeans Clustering
 
 # Importing the dataset
-dataset = read.csv('Data.csv')
+dataset = read.csv('Mall_Customers.csv')
+X = dataset[,4:5]
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+# Using Elbow method to obtain optimal 
+set.seed(6)
+wcss = vector()
+for (i in 1:10) wcss[i] = sum(kmeans(X,i)$withinss)
+# Kmeans retreans an object class kmeans
+# withinss = Vector of within-cluster sum of squares, one component per cluster.
+plot(1:10, wcss, 
+     type = 'b', 
+     main = paste('Cluster of Clients'), 
+     xlab = 'Number of Clusters', 
+     ylab = 'WCSS')
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+# Apply K Means to the dataset
+set.seed(29)
+kmeans = kmeans(X, centers = 5, iter.max = 300, nstart = 10)
+
+# Visualize the Clusters
+library(cluster)
+clusplot(X, 
+         kmeans$cluster,
+         line = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of Clients'),
+         xlab = 'Annual Income',
+         ylab = 'Spending Score')
