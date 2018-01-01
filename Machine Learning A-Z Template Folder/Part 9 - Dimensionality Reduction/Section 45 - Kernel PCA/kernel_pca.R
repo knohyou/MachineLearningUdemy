ae983c692c262cdcd687eaaf3a0a3cf354411b8c
Hyunkoo <knohyou@gmail.com>
2018-01-01 11:57:58 -0500

    Completed Part9 k-fold validation in R

--- Machine Learning A-Z Template Folder/Part 9 - Dimensionality Reduction/Section 45 - Kernel PCA/kernel_pca.R
+++ Machine Learning A-Z Template Folder/Part 9 - Dimensionality Reduction/Section 45 - Kernel PCA/kernel_pca.R
@@ -26,9 +26,10 @@ kpca = kpca(~.,
             features = 2)
 training_set_pca = as.data.frame(predict(kpca, training_set))
 training_set_pca$Purchased = training_set$Purchased
-test_set = as.data.frame(predict(kpca, test_set))
+training_set = training_set_pca
+test_set_pca = as.data.frame(predict(kpca, test_set))
 test_set_pca$Purchased = test_set$Purchased
-
+test_set = test_set_pca
 # Fitting the Regression Model to the dataset
 # Create your regressor here
 classifier = glm(formula = Purchased ~ .,
@@ -51,13 +52,13 @@ set = training_set
 X1 = seq(min(set[,1])-1, max(set[,1])+1, by = 0.01)
 X2 = seq(min(set[,2])-1, max(set[,2])+1, by =0.01)
 grid_set = expand.grid(X1,X2)
-colnames(grid_set) = c('Age','EstimatedSalary')
+colnames(grid_set) = c('V1','V2')
 prob_set = predict(classifier, type = 'response', newdata = grid_set)
 y_grid = ifelse(prob_set > 0.5, 1, 0)
 plot(set[,-3], 
      main = 'Logistic Regression (Training Set)',
-     xlab = 'Age',
-     ylab =  'Estimated Salary',
+     xlab = 'PC1',
+     ylab =  'PC2',
      xlim = range(X1),
      ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -71,13 +72,13 @@ set = test_set
 X1 = seq(min(set[,1])-1, max(set[,1])+1, by = 0.01)
 X2 = seq(min(set[,2])-1, max(set[,2])+1, by =0.01)
 grid_set = expand.grid(X1,X2)
-colnames(grid_set) = c('Age','EstimatedSalary')
+colnames(grid_set) = c('V1','V2')
 prob_set = predict(classifier, type = 'response', newdata = grid_set)
 y_grid = ifelse(prob_set > 0.5, 1, 0)
 plot(set[,-3], 
      main = 'Logistic Regression (Training Set)',
-     xlab = 'Age',
-     ylab =  'Estimated Salary',
+     xlab = 'PC1',
+     ylab =  'PC2',
      xlim = range(X1),
      ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
